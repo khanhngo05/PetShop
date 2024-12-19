688a0814ad39c91e5a56820accc06652645d3788
Bruhlife <ngokhanh1807@gmail.com>
2024-12-19 14:21:14 +0700

    Danh muc

--- pet-shop/src/components/Layout/DefaultLayout/Sidebar/index.js
+++ pet-shop/src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,49 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+import images from '~/assets/images';
+
+const cx = classNames.bind(styles);
+
 function Sidebar() {
-  return <h2>Sidebar</h2>;
+  return (
+    <div className={cx('category__wrap')}>
+      <div className={cx('grid', 'wide')}>
+        <div className={cx('row')}>
+          <div className={cx('col c-12')}>
+            <div className={cx('row')}>
+              <div className={cx('col c-12', 'category__title')}>DANH MỤC</div>
+              <div className={cx('col c-3', 'category__item')}>
+                <button className={cx('category__item-link')}>
+                  <div className={cx('category__item-img')}></div>
+                  <h4>Thú cưng</h4>
+                </button>
+              </div>
+              <div className={cx('col c-3', 'category__item')}>
+                <button className={cx('category__item-link')}>
+                  <div className={cx('category__item-img')}></div>
+                  <h4>Thú cưng</h4>
+                </button>
+              </div>
+              <div className={cx('col c-3', 'category__item')}>
+                <button className={cx('category__item-link')}>
+                  <div className={cx('category__item-img')}></div>
+                  <h4>Thú cưng</h4>
+                </button>
+              </div>
+              <div className={cx('col c-3', 'category__item')}>
+                <button className={cx('category__item-link')}>
+                  <div className={cx('category__item-img')}></div>
+                  <h4>Thú cưng</h4>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
